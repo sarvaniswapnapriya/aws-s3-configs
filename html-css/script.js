e8156d1f559c8451260507b2613b58fc708f4ff4
sarvaniswapnapriya <sarvaniswapnapriya14@gmail.com>
2024-11-11 23:27:03 +0530

    updated the demo

--- html-css/script.js
+++
@@ -1,1 +0,0 @@
-console.log("This is an example JavaScript file.");
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,54 @@
 provider "aws" {
-  region = "eu-north-1" 
+  region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "insecure_s3_bucket" {
-  bucket = "coderabbit-s3-demo"  
-  
-  acl = "public-read"
+resource "aws_s3_bucket" "coderabbit_s3_demo" {
+  bucket = "coderabbit-s3-demo"
+  acl    = "public-read"  # Security Issue: Publicly accessible bucket
 
-  versioning {
-    enabled = true 
+  # Overly permissive bucket policy
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:*",
+        "Resource": [
+          "arn:aws:s3:::coderabbit-s3-demo",
+          "arn:aws:s3:::coderabbit-s3-demo/*"
+        ]
+      }
+    ]
   }
+  POLICY
 
-  logging {
-    target_bucket = "coderabbit-s3-demo"  
-    target_prefix = "logs/"
-    enabled        = false  
-  }
-
+  # Misconfigured CORS settings
   cors_rule {
-    allowed_headers = ["*"]  
-    allowed_methods = ["GET", "POST"]  
-    allowed_origins = ["*"]  
-    max_age_seconds = 3000 
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
   }
 
+  # Logging disabled
+  logging {
+    target_bucket = "coderabbit-s3-demo"
+    target_prefix = "logs/"
+  }
+
+  # Versioning is disabled
+  versioning {
+    enabled = false
+  }
+
+  # Tags
   tags = {
     Environment = "Development"
   }
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.insecure_s3_bucket.bucket
+  value = aws_s3_bucket.coderabbit_s3_demo.bucket
 }
